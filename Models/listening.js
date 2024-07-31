5cd6328216a52751901db032ebda929bb252f13f
Ronit Prasad <115333372+ronitprasad23@users.noreply.github.com>
2024-07-31 22:28:40 +0530

    Update listening.js

--- Models/listening.js
+++ Models/listening.js
@@ -1,1 +1,51 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review");
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+
+    image: {
+        url: String,
+        filename: String,
+    },
+    price: Number,
+    location: String,
+    country: String,
+    reviews: [
+        {   
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ],
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    geometry: {
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+            required: true,
+          },
+          coordinates: {
+            type: [Number],
+            required: true, 
+          },
+    },
+});
+
+listingSchema.post("findOneAndDelete", async(listings) => {
+    if (listings) {
+       await Review.deleteMany({reviews : {$in: listings.reviews}});
+    }
+});
+    
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
 
